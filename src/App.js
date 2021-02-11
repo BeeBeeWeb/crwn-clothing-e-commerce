a36ce67eda9b730f73bb1f8f1027d65c3ba5720e
bhnbhushan <babar.bhushan@gmail.com>
2021-02-11 20:20:08 +0530

    added redux, added userReducer in Header and App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from "react-redux";
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -7,37 +8,28 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          console.log(snapshot.data());
-          this.setState({
-            currentUser: {
+          // console.log(snapshot.data());
+          setCurrentUser({
               id: snapshot.id,
               ...snapshot.data()
-            }
-          })
+            })
         });
       } else {
         // userAuth is return null when user signs out
-        this.setState({
-          currentUser: userAuth
-        })
+        setCurrentUser(userAuth)
       }
     });
   }
@@ -49,7 +41,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}></Route>
           <Route exact path='/shop' component={ShopPage}></Route>
@@ -60,4 +52,8 @@ class App extends React.Component {
   };
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+// state here is current state, 
+// and action is object with 'type' and 'payload' properties
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
